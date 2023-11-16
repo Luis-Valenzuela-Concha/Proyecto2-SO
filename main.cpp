45c8d1e5862c840f70f4e6ad6b91c1e27cb6d13c
Bruno Arce R <brunoarce0@gmail.com>
2023-11-15 23:04:43 -0300

    procesarGenoma implemented

--- main.cpp
+++ main.cpp
@@ -18,7 +18,23 @@ vector<string> copiarArchivoString(string nombreArchivo) {
     return vec;
 }
 
+bool procesarGenoma(vector<string> genoma, float umbral) {
+    int CG = 0;
+    int total = 0;
+    for (int i = 0; i < genoma.size(); i++) {
+        string linea = genoma[i];
+        for (int j = 0; j < linea.size(); j++) {
+            if (linea[j] == 'C' || linea[j] == 'G') CG++;
+            total++;
+        }
+    }
+    return CG / (float)total >= umbral;
+}
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    vector<string> vec = copiarArchivoString("GCF_000717965.1_ASM71796v1_genomic.fna");
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << endl;
+    }
     return 0;
 }
